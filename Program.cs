a4adfff614d54f91f6fd862b96fdd63d892878a7
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-11-14 11:24:39 +0600

    Задание 6.2.2

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Module6
+﻿using System.Drawing;
+using System.Xml.Linq;
+
+namespace Module6
+    ///Задание 6.2.2
+    ///Добавьте в класс Pen, указанный ниже, 2 конструктора:
+    /// - без параметров, когда поля color и cost заполняются значениями Черный и 100.
+    /// - с 2 параметрами: penColor и penCost.
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+        }
+    }
+    class Pen
+    {
+        public string color;
+        public int cost;
+
+        // Конструктор 1
+        public Pen()
+        {
+            color = "Черный";
+            cost = 100;
+        }
+        // Конструктор 2
+        public Pen(string penColor, int penCost)
+        {
+            color = penColor;
+            cost = penCost;
         }
     }
 }
