01e891668e712929c615b9e15660649d410e26ff
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-11-15 10:54:39 +0600

    Задание 6.3.2

--- Program.cs
+++ Program.cs
@@ -3,44 +3,33 @@ using System.Xml.Linq;
 
 namespace Module6
 {
-    /// Задание 6.3.1
-    /// Напишите такой код, который бы при типе компании, равному типу "Банк", и городе "Санкт-Петербург" выводил в консоль сообщение 
-    /// "У банка ??? есть отделение в Санкт-Петербурге", где вместо "???" выводилось бы название компании.
-    ///Если у компании нет названия, вместо него должно быть "Неизвестная компания".
+    //Задание 6.3.2
+    //Для класса Bus реализуйте метод PrintStatus,
+    //который будет сообщать о количестве пассажиров в автобусе, если они есть.
+    //Или сообщать, что автобус пуст.
 
     class Program
     {
         static void Main(string[] args)
         {
-            var department = GetCurrentDepartment();
-            if (department?.Company?.Type == "Банк" && department?.City?.Name == "Санкт-Петербург")
+            
+        }
+    }
+    class Bus
+    {
+        public int? Load;
+
+        public void PrintStatus()
+        {
+            if (Load.HasValue)
             {
-                Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", department?.Company?.Name ?? "Неизвестная компания");
+                Console.WriteLine("В авбтобусе {0} пассажиров", Load.Value);
+            }
+            else
+            {
+                Console.WriteLine("Автобус пуст!");
             }
         }
 
-        static Department GetCurrentDepartment()
-        {
-            // logic
-        }
     }
-
-    class Company
-    {
-        public string Type;
-        public string Name;
-    }
-
-    class Department
-    {
-        public Company Company;
-        public City City;
-    }
-
-    class City
-    {
-        public string Name;
-    }
-
-
 }
