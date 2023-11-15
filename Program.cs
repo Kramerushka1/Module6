a32127e2e3eaed73caf9b8d818812f4b89632625
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-11-15 10:50:10 +0600

    Задание 6.3.1

--- Program.cs
+++ Program.cs
@@ -2,52 +2,45 @@
 using System.Xml.Linq;
 
 namespace Module6
-    ///Задание 6.2.8
-    ///Создайте класс Rectangle для представления прямоугольников удовлетворяющий следующим требованиям:
-    ///- Класс должен содержать целочисленные поля для сторон a и b.
-    ///- Класс должен содержать метод Square, возвращающий площадь прямоугольника(произведение сторон).
-    ///- Класс должен содержать 3 конструктора: 
-    ///  - с 2 параметрами, когда a != b, 
-    ///  - с 1 параметром, когда a == b, 
-    ///  - и конструктор без параметров по умолчанию, в котором стороны будут заполняться как a = 6, b = 4.
-
 {
-    internal class Program
+    /// Задание 6.3.1
+    /// Напишите такой код, который бы при типе компании, равному типу "Банк", и городе "Санкт-Петербург" выводил в консоль сообщение 
+    /// "У банка ??? есть отделение в Санкт-Петербурге", где вместо "???" выводилось бы название компании.
+    ///Если у компании нет названия, вместо него должно быть "Неизвестная компания".
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var department = GetCurrentDepartment();
+            if (department?.Company?.Type == "Банк" && department?.City?.Name == "Санкт-Петербург")
+            {
+                Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", department?.Company?.Name ?? "Неизвестная компания");
+            }
+        }
+
+        static Department GetCurrentDepartment()
+        {
+            // logic
         }
     }
-    class Rectangle
+
+    class Company
     {
-        public int a;
-        public int b;
-
-        // Конструктор 1
-        public Rectangle(int num1, int num2)
-        {
-            a = num1;
-            b = num2;
-        }
-        
-        // Конструктор 2
-        public Rectangle(int num)
-        {
-            a = num;
-            b = num;
-        }
-        
-        // Конструктор 2
-        public Rectangle()
-        {
-            a = 6;
-            b = 4;
-        }
-        public int Square()
-        {
-            return a * b;
-        }
-
+        public string Type;
+        public string Name;
     }
+
+    class Department
+    {
+        public Company Company;
+        public City City;
+    }
+
+    class City
+    {
+        public string Name;
+    }
+
+
 }
